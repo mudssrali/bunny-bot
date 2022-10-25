2e51b84df229f47becb009727b9de9079cdbe081
Mudassar <mudassar.elley@gmail.com>
2022-10-25 23:38:13 +0500

    Coin Gecko API (#2)
    * add coin gecko config
    
    * update mix deps
    
    * build tesla client with dynamic config
    
    * add helper functions to interact with coin gecko
    
    * update README.md

---
+++ lib/crypto_bunny/coin_gecko.ex
@@ -0,0 +1,83 @@
+defmodule CryptoBunny.CoinGecko do
+  @moduledoc """
+  Provides helper functions to interact with CoinGecko API
+  """
+
+  alias CryptoBunny.Utils.TeslaClient
+  require Logger
+
+  @currency "usd"
+  @days_ago 14
+  @data_interval "daily"
+
+  @doc """
+  Searches for coins, categories and markets listed on CoinGecko ordered by largest Market Cap first
+  """
+  @spec search(query :: String.t()) :: {:ok, [map()]} | {:error, any()}
+  def search(query) when is_binary(query) do
+    client = TeslaClient.client(:coin_gecko)
+
+    case Tesla.get(client, "/search", query: [query: query]) do
+      {:ok, response} ->
+        {:ok, response.body}
+
+      {:error, reason} = error ->
+        Logger.error(fn -> "coins search by name: " <> inspect(reason) end)
+
+        error
+    end
+  end
+
+  def search(_), do: []
+
+  @doc """
+  Get current data (name, price, market, ... including exchange tickers) for a coin
+  """
+  @spec get_by_id(id :: String.t()) :: {:ok, map()} | {:error, any()}
+  def get_by_id(id) when is_binary(id) do
+    client = TeslaClient.client(:coin_gecko)
+
+    path = "/coins/#{id}"
+
+    case Tesla.get(client, path) do
+      {:ok, response} ->
+        {:ok, response.body}
+
+      {:error, reason} = error ->
+        Logger.error(fn -> "get coin by id: " <> inspect(reason) end)
+        error
+    end
+  end
+
+  def get_by_id(_), do: nil
+
+  @doc """
+  Get historical market data include price, market cap, and 24h volume (granularity auto) for a coin
+  """
+  @spec get_market_chart(
+          id :: String.t(),
+          currency :: String.t(),
+          days :: non_neg_integer(),
+          interval :: String.t()
+        ) :: {:ok, map()} | {:error, any()}
+  def get_market_chart(id, currency \\ @currency, days \\ @days_ago, interval \\ @data_interval) do
+    client = TeslaClient.client(:coin_gecko)
+
+    path = "/coins/#{id}/market_chart"
+
+    case Tesla.get(client, path,
+           query: [
+             vs_currency: currency,
+             days: days,
+             interval: interval
+           ]
+         ) do
+      {:ok, response} ->
+        {:ok, response.body}
+
+      {:error, reason} = error ->
+        Logger.error(fn -> "coin historical market data: " <> inspect(reason) end)
+        error
+    end
+  end
+end
---
+++ lib/crypto_bunny/utils/tesla_client.ex
@@ -0,0 +1,46 @@
+defmodule CryptoBunny.Utils.TeslaClient do
+  @moduledoc """
+  Provides helper to build tesla client with dynamic configuration
+  """
+  @recv_timeout 30_000
+
+  @doc """
+  returns a tesla client for specified configuration
+  """
+  @spec client(:coin_gecko | nil, timeout :: non_neg_integer()) :: Tesla.Client.t()
+  def client(type, timeout \\ @recv_timeout)
+
+  def client(type, timeout) when type == :coin_gecko do
+    coin_gecko = Application.get_env(:crypto_bunny, :coin_gecko)
+    base_url = coin_gecko.base_url <> "/" <> coin_gecko.api_version
+    new(base_url, timeout)
+  end
+
+  def client(type, timeout) when type == nil do
+    Tesla.client(
+      [
+        {Tesla.Middleware.JSON, engine: Jason},
+        {Tesla.Middleware.Timeout, timeout: timeout}
+      ],
+      {
+        Tesla.Adapter.Hackney,
+        ssl_options: [verify: :verify_none], recv_timeout: timeout
+      }
+    )
+  end
+
+  # Builds tesla client with runtime arguments
+  defp new(base_url, timeout) do
+    Tesla.client(
+      [
+        {Tesla.Middleware.BaseUrl, base_url},
+        {Tesla.Middleware.JSON, engine: Jason},
+        {Tesla.Middleware.Timeout, timeout: timeout}
+      ],
+      {
+        Tesla.Adapter.Hackney,
+        ssl_options: [verify: :verify_none], recv_timeout: timeout
+      }
+    )
+  end
+end
