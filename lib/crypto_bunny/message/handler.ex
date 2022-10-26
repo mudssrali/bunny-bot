9c8e25ad0d81aa74ee681c5c7338031107eb5ef5
Mudassar <mudassar.elley@gmail.com>
2022-10-26 15:28:51 +0500

    handle message to get coin's historica price data

--- lib/crypto_bunny/message/handler.ex
+++ lib/crypto_bunny/message/handler.ex
@@ -6,6 +6,8 @@ defmodule CryptoBunny.Message.Handler do
   alias CryptoBunny.Bot
   alias CryptoBunny.Message
   alias CryptoBunny.Message.Templates
+  alias CryptoBunny.CoinGecko
+  alias CryptoBunny.Utils
 
   require Logger
 
@@ -33,6 +35,39 @@ defmodule CryptoBunny.Message.Handler do
     request_coins_search_method(event)
   end
 
+  # Handles message for coin search by ID
+  def handle_message(%{"text" => "ID_" <> coin_id}, event) do
+    Task.start(fn ->
+      message = "Give us a moment, fetching historical price data for #{coin_id} coin"
+
+      event
+      |> Templates.text(message)
+      |> Bot.send_message()
+    end)
+
+    case CoinGecko.get_market_chart(coin_id) do
+      {:ok, historical_data} ->
+        prices = Map.get(historical_data, "prices", [])
+        prices_string = get_prices_string(prices)
+
+        message =
+          if prices == [] do
+            "Historical price data not found for coin with id '#{coin_id}', Try again."
+          else
+            "Historical price data for coin with id '#{coin_id}' for past 14 days. \n\n" <>
+              prices_string
+          end
+
+        event
+        |> Templates.text(message)
+        |> Bot.send_message()
+
+      {:error, _reason} ->
+        body = Templates.text(event, "Something went wrong, Try again!")
+        Bot.send_message(body)
+    end
+  end
+
   def handle_message(_message, event) do
     greetings = Message.greeting()
 
@@ -49,11 +84,21 @@ defmodule CryptoBunny.Message.Handler do
   Handles postback event
   """
   @spec handle_postback(postback :: map(), event :: event()) :: :ok | :error
-  def handle_postback(%{"payload" => "coins_search_by_" <> selected_coins_search_method}, event) do
-    Logger.info("selected coins search method: #{selected_coins_search_method}")
+  def handle_postback(%{"payload" => "coins_search_by_" <> selected_method}, event) do
+    Logger.info("selected coins search method: #{selected_method}")
+
+    search_method =
+      case selected_method do
+        "id" ->
+          "Please write Coin ID in format: ID_[coin_id] " <>
+            "e.g. ID_bitcoin to get market historical data"
+
+        "name" ->
+          "Please write Coins Name search in format: CN_[name] e.g. CN_bitcoin"
+      end
 
     event
-    |> Templates.text("Thank you, for your selection, please wait!")
+    |> Templates.text("Thank you, for your selection!\n" <> search_method)
     |> Bot.send_message()
   end
 
@@ -71,5 +116,15 @@ defmodule CryptoBunny.Message.Handler do
 
     # Send postback message
     Bot.send_message(coin_search_method_template)
+  end
+
+  defp get_prices_string(prices) do
+    Enum.map(prices, fn [date, rate] ->
+      date = Utils.Date.to_iso(date)
+      rate = Utils.Currency.roundoff(rate)
+
+      "#{date}: $#{rate}"
+    end)
+    |> Enum.join("\n")
   end
 end
